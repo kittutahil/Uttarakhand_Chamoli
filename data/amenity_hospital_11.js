fd9256c2c0885ad099262dfa35bdc2612739bfdd
kittutahil <94543731+kittutahil@users.noreply.github.com>
2021-11-17 23:31:55 +0530

    Add files via upload

---
+++ data/amenity_hospital_11.js
@@ -0,0 +1,15 @@
+var json_amenity_hospital_11 = {
+"type": "FeatureCollection",
+"name": "amenity_hospital_11",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "amenity": "hospital", "name": "Nandprayag Governnment Hospital", "addr_distr": "Chamoli", "addr_full": "Near Sulbh International, Main Market Nandprayag" }, "geometry": { "type": "Point", "coordinates": [ 79.3210273, 30.3316591 ] } },
+{ "type": "Feature", "properties": { "amenity": "hospital", "name": "Government Hospital, Karnaprayag", "addr_distr": "Chamoli", "addr_full": "NH 109, Karnaprayag" }, "geometry": { "type": "Point", "coordinates": [ 79.2149926, 30.2602971 ] } },
+{ "type": "Feature", "properties": { "amenity": "hospital", "name": "Parkhal Hospital", "addr_distr": "Chamoli", "addr_full": "Ased, Parkhal" }, "geometry": { "type": "Point", "coordinates": [ 79.4012884, 30.1407397 ] } },
+{ "type": "Feature", "properties": { "amenity": "hospital", "name": "Trauma Centre", "addr_distr": "Chamoli", "addr_full": "Badrinath Road, Karnaprayag" }, "geometry": { "type": "Point", "coordinates": [ 79.2151662, 30.2632645 ] } },
+{ "type": "Feature", "properties": { "amenity": "hospital", "name": "Governnment Hospital, Tuneda", "addr_distr": "Chamoli", "addr_full": "Almora-Bageshwar-Gopeshwar Road, Chopata ,Tuneda" }, "geometry": { "type": "Point", "coordinates": [ 79.3723226, 30.1809855 ] } },
+{ "type": "Feature", "properties": { "amenity": "hospital", "name": "Pipalkoti SD Ground Hospital", "addr_distr": "Chamoli", "addr_full": "Pipalkoti" }, "geometry": { "type": "Point", "coordinates": [ 79.4302887, 30.4334554 ] } },
+{ "type": "Feature", "properties": { "amenity": "hospital", "name": "ITBP Hospital", "addr_distr": "Chamoli", "addr_full": "Bhat Gaon" }, "geometry": { "type": "Point", "coordinates": [ 79.145176, 30.287139 ] } },
+{ "type": "Feature", "properties": { "amenity": "hospital", "name": "Swami Vivekanand Dharmarth Chikitsalay", "addr_distr": "Chamoli", "addr_full": "Pipalkoti" }, "geometry": { "type": "Point", "coordinates": [ 79.4268799, 30.4284287 ] } }
+]
+}
